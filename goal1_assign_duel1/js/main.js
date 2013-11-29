fe8807d64f98c07837fa4912c1ae79281b0bb914
julieclucas <julieclucas@fullsail.edu>
2013-11-29 12:05:53 -0500

    function fight

---
+++ goal1_assign_duel1/js/main.js
@@ -0,0 +1,64 @@
+/*
+    Name: Julie Veal
+    Date: 11/29/13
+    Assignment: Goal1: Assignment: Duel1
+*/
+
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!");
+
+//players names
+
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //initiate round
+    var round=1;
+
+    function fight(){
+       console.log('in the fight function');
+        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);     //string can
+
+        for (var i = 0; i < 10; i++){
+            //random  formula is - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+
+            //check for victor
+            var result = winnerCheck();
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+
+            } else{
+                alert(result);
+                break;
+            }
+
+        }
+    }
+
+
+
+
+})();
